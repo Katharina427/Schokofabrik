cad92f6620e333d43610f8078ea2a3b1471d0611
Helena05M <helena.mederle@gmail.com>
2025-06-05 13:18:51 +0200

    Design

--- src/main/java/Schokofabrik.java
+++ src/main/java/Schokofabrik.java
@@ -2,4 +2,23 @@ import javax.swing.*;
 
 public class Schokofabrik extends JFrame{
     private JPanel myPanel;
+    private JRadioButton rbVollmilch;
+    private JRadioButton rbZartbitter;
+    private JRadioButton rbWeiß;
+    private JLabel lblSorte;
+    private JRadioButton himbeerenRadioButton;
+    private JRadioButton rbKekse;
+    private JRadioButton rbNüsse;
+    private JRadioButton rbSalzbrezeln;
+    private JRadioButton rbSamrties;
+    private JLabel lblGröße;
+    private JRadioButton rbXXL;
+    private JRadioButton rbTafel;
+    private JRadioButton rbRiegel;
+    private JLabel lblPreis;
+    private JTextField tfPreis;
+    private JCheckBox cbVegan;
+    private JLabel lblVegan;
+    private JButton speichernButton;
+    private JButton berechnenButton;
 }
